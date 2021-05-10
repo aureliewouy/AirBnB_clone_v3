91d12217037ef42983140258b2c2962a108f872a
Natalia Ponsard <2503@holbertonschool.com>
2021-05-10 11:10:34 +0000

    try another version for /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 
-objs = {"amenities": "Amenity", "cities": "City", "places": "Place",
-        "reviews": "Review", "states": "State", "users": "User"}
+# objs = {"amenities": "Amenity", "cities": "City", "places": "Place",
+#        "reviews": "Review", "states": "State", "users": "User"}
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -17,7 +17,12 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ Returns the count method """
-    dict_type = {}
-    for key, value in objs.items():
-        dict_type[key] = storage.count(value)
-    return jsonify(dict_type)
+#   dict_type = {}
+#   for key, value in objs.items():
+#       dict_type[key] = storage.count(value)
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
