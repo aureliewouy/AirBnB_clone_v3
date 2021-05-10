2b4e3fd3b2cb07badadfa8c27ed6115ab7031d43
aureliewouy <aureliecedia@yahoo.com>
2021-05-10 09:03:17 +0000

    pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,8 @@
 """
 Package Blueprint
 """
-
-
+from api.v1.views.index import *
+from api.v1.views.states import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
