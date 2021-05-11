e203806e439f6e7ced625a60d925be50dd91eb96
aureliewouy <aureliecedia@yahoo.com>
2021-05-11 09:22:24 +0000

    update places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,9 +8,11 @@ from models import storage
 from api.v1.views import app_views
 from models.place import Place
 from models.city import City
+from models.user import User
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
 def get_city_by_place(city_id):
     """retrieves the list of all State objects with info about states"""
     city = storage.get(City, city_id)
@@ -32,43 +34,48 @@ def get_place(place_id):
         return jsonify(place.to_dict())
     else:
         abort(404)
-"""
+
+
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
-    delete place obj by id
+    """delete place obj by id"""
     place = storage.get(Place, place_id)
     if place:
         place.delete()
-        place.save()
+        storage.save()
         return jsonify({}), 200
     else:
         abort(404)
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def create_placesobj(city_id):
-    creates a State
+    """creates a place"""
     # request.get_json() - converts the JSON object into Python data
     kwargs = request.get_json()
     if not kwargs:
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'user_id' not in kwargs:
         return make_response(jsonify({'error': 'Missing user_id'}), 400)
+    if storage.get(City, city_id) is None:
+        abort(404)
     if storage.get(User, kwargs["user_id"]) is None:
         abort(404)
-    if storage.get("name") is None:
+    if kwargs.get("name") is None:
         return "Missing name", 400
     # obj = class(**kwargs)
-    place = Place()
-    for k, v in kwargs.items():
-        setattr(place, k, v)
-    setattr(place, "city_id", city_id)
+    kwargs['city_id'] = city_id
+    place = Place(**kwargs)
     place.save()
-    return jsonify(jsonify(place.to_dict()), 201
+    return make_response(jsonify(place.to_dict()), 201)
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
 def put_placeobj(place_id):
-    update place obj by id
+    """update place obj by id"""
     ignore_keys = ['id', 'created_at', 'updated_at', 'city_id', 'user_id']
     place = storage.get(Place, place_id)
     if place is None:
@@ -80,5 +87,4 @@ def put_placeobj(place_id):
         if key not in ignore_keys:
             setattr(place, key, value)
     place.save()
-    return jsonify(state.to_dict())
-"""
+    return jsonify(place.to_dict())
